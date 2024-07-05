19f963e80c38abc7877de7b7ad46adc18eea63a4
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-07-05 21:27:33 +0200

    add commit

--- task-1.js
+++ task-1.js
@@ -2,9 +2,9 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
 }
 if (totalPrice > customerCredits) {
-  console.log(`Insufficient funds!`);
+  console.log("Insufficient funds!");
 } else {
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  return "You ordered ${quantity} droids worth ${totalPrice} credits!";
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
